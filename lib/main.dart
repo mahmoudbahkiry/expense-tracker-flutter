82aa475e88d8f2f6ec4506985a79a450857e14e9
omar-omar-om <93835031+omar-omar-om@users.noreply.github.com>
2023-12-02 18:24:28 +0000

    solving errors

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ), // ThemeData
-      home: SplashScreen(),
+      home:const Auth(),
     ); // MaterialApp
  }
 }
